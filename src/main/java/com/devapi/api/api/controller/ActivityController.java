2b8cb48f0295576365b4f82ea2f40d0be8112390
Denerson Berudio <denerson2808@hotmail.com>
2022-12-18 21:20:03 -0300

    atualizando atividades

--- src/main/java/com/devapi/api/api/controller/ActivityController.java
+++ src/main/java/com/devapi/api/api/controller/ActivityController.java
@@ -10,7 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/activities")
@@ -30,5 +32,29 @@ public class ActivityController {
     @GetMapping
     public List<Activity> Listar(){
         return repository.findAll();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Activity> editar(@PathVariable("id") Long atividadeId, @RequestBody Activity atividadeInfo) {
+        Activity atividade = repository.findById(atividadeId).get();
+
+        atividade.setNome(atividadeInfo.getNome());
+        atividade.setDescricao(atividadeInfo.getDescricao());
+        atividade.setData(atividadeInfo.getData());
+        atividade.setHorario_inicial(atividadeInfo.getHorario_inicial());
+        atividade.setHorario_final(atividadeInfo.getHorario_final());
+
+        final Activity activityAtt = repository.save(atividade);
+        return ResponseEntity.ok(activityAtt);
+    }
+
+    @DeleteMapping("/{id}")
+    public Map<String, Boolean> deletar(@PathVariable("id") Long atividadeId) {
+        Activity atividade = repository.findById(atividadeId).get();
+
+        repository.delete(atividade);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("Atividade Deletada", Boolean.TRUE);
+        return response;
     }
 }
